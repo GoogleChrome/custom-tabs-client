fd52c2d90732d9a31a0bdf883d11d16f1d7b5243
yusufo <yusufo@chromium.org>
2016-05-13 12:36:08 -0700

    Add componentName to the createdummySession call
    Looks like a null component name is not enough for testing this
    thorougly. Adding a component name to avoid a crush during intent
    construction.
    
    BUG=522099
    
    Review-Url: https://codereview.chromium.org/1973103003

--- customtabs/src/android/support/customtabs/CustomTabsSession.java
+++ customtabs/src/android/support/customtabs/CustomTabsSession.java
@@ -43,10 +43,11 @@ public final class CustomTabsSession {
      * Provides browsers a way to generate a dummy {@link CustomTabsSession} for testing
      * purposes.
      *
+     * @param componentName The component the session should be created for.
      * @return A dummy session with no functionality.
      */
-    public static CustomTabsSession createDummySessionForTesting() {
-        return new CustomTabsSession(null, new DummyCallback(), null);
+    public static CustomTabsSession createDummySessionForTesting(ComponentName componentName) {
+        return new CustomTabsSession(null, new DummyCallback(), componentName);
     }
 
     /* package */ CustomTabsSession(
