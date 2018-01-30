a44c31587e884771dcc377cf7b667480a8b2da77
Lei Tian <ltian@chromium.org>
2018-01-30 15:40:01 -0800

    Sync Browser Actions changes on Android support lib
    Sync the latest changes of Browser Actions in Android support lib.
    
    Bug: 789806
    Change-Id: Id452b801a2604b113a8f94774afa109bfb955c9e

--- customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuUi.java
+++ customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuUi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,16 @@
 
 package android.support.customtabs.browseractions;
 
+import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
+
 import android.app.PendingIntent;
 import android.app.PendingIntent.CanceledException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
+import android.support.annotation.RestrictTo;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.widget.TextViewCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,12 +39,21 @@ import java.util.List;
 /**
  * The class to show fallback menu for Browser Actions if no provider is available.
  */
-public class BrowserActionsFallbackMenuUi implements AdapterView.OnItemClickListener {
-    private static final String TAG = "BrowserActionsFallbackMenuUi";
+class BrowserActionsFallbackMenuUi implements AdapterView.OnItemClickListener {
+    /** @hide */
+    @VisibleForTesting
+    @RestrictTo(LIBRARY_GROUP)
+    interface BrowserActionsFallMenuUiListener {
+        void onMenuShown(View view);
+    }
+
+    private static final String TAG = "BrowserActionskMenuUi";
 
     private final Context mContext;
     private final Uri mUri;
     private final List<BrowserActionItem> mMenuItems;
+
+    private BrowserActionsFallMenuUiListener mMenuUiListener;
 
     private BrowserActionsFallbackMenuDialog mBrowserActionsDialog;
 
@@ -47,21 +62,36 @@ public class BrowserActionsFallbackMenuUi implements AdapterView.OnItemClickList
      * @param uri The uri which users click to trigger the menu.
      * @param menuItems The custom menu items shown in the menu.
      */
-    public BrowserActionsFallbackMenuUi(
+    BrowserActionsFallbackMenuUi(
             Context context, Uri uri, List<BrowserActionItem> menuItems) {
         mContext = context;
         mUri = uri;
         mMenuItems = menuItems;
     }
 
+    /** @hide */
+    @VisibleForTesting
+    @RestrictTo(LIBRARY_GROUP)
+    void setMenuUiListener(BrowserActionsFallMenuUiListener menuUiListener) {
+        mMenuUiListener = menuUiListener;
+    }
+
     /**
      * Shows the fallback menu.
      */
     public void displayMenu() {
-        View view = LayoutInflater.from(mContext).inflate(
+        final View view = LayoutInflater.from(mContext).inflate(
                 R.layout.browser_actions_context_menu_page, null);
         mBrowserActionsDialog = new BrowserActionsFallbackMenuDialog(mContext, initMenuView(view));
         mBrowserActionsDialog.setContentView(view);
+        if (mMenuUiListener != null) {
+            mBrowserActionsDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialogInterface) {
+                    mMenuUiListener.onMenuShown(view);
+                }
+            });
+        }
         mBrowserActionsDialog.show();
     }
 
@@ -69,12 +99,12 @@ public class BrowserActionsFallbackMenuUi implements AdapterView.OnItemClickList
         BrowserActionsFallbackMenuView menuView =
                 (BrowserActionsFallbackMenuView) view.findViewById(R.id.browser_actions_menu_view);
 
-        TextView urlTextView = (TextView) view.findViewById(R.id.browser_actions_header_text);
+        final TextView urlTextView = (TextView) view.findViewById(R.id.browser_actions_header_text);
         urlTextView.setText(mUri.toString());
         urlTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (urlTextView.getMaxLines() == Integer.MAX_VALUE) {
+                if (TextViewCompat.getMaxLines(urlTextView) == Integer.MAX_VALUE) {
                     urlTextView.setMaxLines(1);
                     urlTextView.setEllipsize(TextUtils.TruncateAt.END);
                 } else {
