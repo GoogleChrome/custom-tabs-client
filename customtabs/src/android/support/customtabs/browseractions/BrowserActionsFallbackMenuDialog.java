11160d765f0448083fc0ad7d2059c6d2ce3002b7
Lei Tian <ltian@chromium.org>
2018-01-30 15:40:01 -0800

    Sync Browser Actions changes on Android support lib
    Sync the latest changes of Browser Actions in Android support lib.
    
    Bug: 789806
    Change-Id: Id452b801a2604b113a8f94774afa109bfb955c9e
    Reviewed-on: https://chromium-review.googlesource.com/894728
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Lei Tian <ltian@chromium.org>

--- customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuDialog.java
+++ customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,13 +31,13 @@ import android.view.Window;
  * The dialog class showing the context menu and ensures proper animation is played upon calling
  * {@link #show()} and {@link #dismiss()}.
  */
-public class BrowserActionsFallbackMenuDialog extends Dialog {
+class BrowserActionsFallbackMenuDialog extends Dialog {
     private static final long ENTER_ANIMATION_DURATION_MS = 250;
     // Exit animation duration should be set to 60% of the enter animation duration.
     private static final long EXIT_ANIMATION_DURATION_MS = 150;
     private final View mContentView;
 
-    public BrowserActionsFallbackMenuDialog(Context context, View contentView) {
+    BrowserActionsFallbackMenuDialog(Context context, View contentView) {
         super(context, android.support.v7.appcompat.R.style.Theme_AppCompat_Light_Dialog);
         mContentView = contentView;
     }
@@ -64,7 +64,7 @@ public class BrowserActionsFallbackMenuDialog extends Dialog {
         startAnimation(false);
     }
 
-    private void startAnimation(boolean isEnterAnimation) {
+    private void startAnimation(final boolean isEnterAnimation) {
         float from = isEnterAnimation ? 0f : 1f;
         float to = isEnterAnimation ? 1f : 0f;
         long duration = isEnterAnimation ? ENTER_ANIMATION_DURATION_MS : EXIT_ANIMATION_DURATION_MS;
