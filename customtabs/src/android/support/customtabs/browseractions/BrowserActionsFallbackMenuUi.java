8226e49d5737e3db5572c40cdd3d55f1bc80ba76
massongit <suzukimasaya428@gmail.com>
2018-02-24 14:10:22 +0900

    I added "import android.support.customtabs.R" and added the final qualifier to viewHolder so that the Android application can use the customtabs package and the shared package.
    Change-Id: I95d3de3d5836312de7e0406b21bb3dd21f39064a

--- customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuUi.java
+++ customtabs/src/android/support/customtabs/browseractions/BrowserActionsFallbackMenuUi.java
@@ -25,6 +25,7 @@ import android.content.DialogInterface;
 import android.net.Uri;
 import android.support.annotation.RestrictTo;
 import android.support.annotation.VisibleForTesting;
+import android.support.customtabs.R;
 import android.support.v4.widget.TextViewCompat;
 import android.text.TextUtils;
 import android.util.Log;
