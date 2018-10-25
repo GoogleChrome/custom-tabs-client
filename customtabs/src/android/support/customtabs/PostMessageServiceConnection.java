c813ed8fcc4ece1838dba8dddf6d8ca39d6c6785
David Barker <barkerd@google.com>
2018-10-25 17:23:14 +0100

    Pull methods and behaviour from PostMessageHandler into PostMessageServiceConnection
    This is a pure refactoring which doesn't affect the current behaviour of
    PostMessageServiceConnection.
    
    At the same time, make PostMessageServiceConnection implement a new
    PostMessageBackend interface which will make it possible to abstract away the
    service connection in the Chromium implementation of the postMessage API.
    
    Bug: 898840
    Change-Id: Ied0c102e53dff893a7806f9e75ae409b9cd53755
    Reviewed-on: https://chromium-review.googlesource.com/c/1299016
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: David Barker <barkerd@google.com>

--- customtabs/src/android/support/customtabs/PostMessageServiceConnection.java
+++ customtabs/src/android/support/customtabs/PostMessageServiceConnection.java
@@ -28,13 +28,25 @@ import android.os.RemoteException;
  * A {@link ServiceConnection} for Custom Tabs providers to use while connecting to a
  * {@link PostMessageService} on the client side.
  */
-public abstract class PostMessageServiceConnection implements ServiceConnection {
+public class PostMessageServiceConnection implements PostMessageBackend, ServiceConnection {
     private final Object mLock = new Object();
     private final ICustomTabsCallback mSessionBinder;
     private IPostMessageService mService;
+    private String mPackageName;
+    // Indicates that a message channel has been opened. We're ready to post messages once this is
+    // true and we've connected to the {@link PostMessageService}.
+    private boolean mMessageChannelCreated;
 
     public PostMessageServiceConnection(CustomTabsSessionToken session) {
         mSessionBinder = ICustomTabsCallback.Stub.asInterface(session.getCallbackBinder());
+    }
+
+    /**
+     * Sets the package name unique to the session.
+     * @param packageName The package name for the client app for the owning session.
+     */
+    public void setPackageName(String packageName) {
+        mPackageName = packageName;
     }
 
     /**
@@ -52,11 +64,24 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
     }
 
     /**
+     * See
+     * {@link PostMessageServiceConnection#bindSessionToPostMessageService(Context, String)}.
+     * Attempts to bind with the package name set during initialization.
+     */
+    public boolean bindSessionToPostMessageService(Context appContext) {
+        return bindSessionToPostMessageService(appContext, mPackageName);
+    }
+
+    private boolean isBoundToService() {
+        return mService != null;
+    }
+
+    /**
      * Unbinds this service connection from the given context.
      * @param context The context to be unbound from.
      */
     public void unbindFromContext(Context context) {
-        context.unbindService(this);
+        if (isBoundToService()) context.unbindService(this);
     }
 
     @Override
@@ -71,6 +96,22 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
         onPostMessageServiceDisconnected();
     }
 
+    @Override
+    public final boolean onNotifyMessageChannelReady(Bundle extras) {
+        return notifyMessageChannelReady(extras);
+    }
+
+    /**
+     * Records that the message channel has been created and calls through to {@link
+     * #notifyMessageChannelReadyInternal}, which will notify the service if it's connected.
+     * @param extras Unused.
+     * @return Whether the notification was sent successfully.
+     */
+    public final boolean notifyMessageChannelReady(Bundle extras) {
+        mMessageChannelCreated = true;
+        return notifyMessageChannelReadyInternal(extras);
+    }
+
     /**
      * Notifies the client that the postMessage channel requested with
      * {@link CustomTabsService#requestPostMessageChannel(
@@ -81,8 +122,8 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
      * @param extras Reserved for future use.
      * @return Whether the notification was sent to the remote successfully.
      */
-    public final boolean notifyMessageChannelReady(Bundle extras) {
-        if (mService == null) return false;
+    private final boolean notifyMessageChannelReadyInternal(Bundle extras) {
+        if (!isBoundToService()) return false;
         synchronized (mLock) {
             try {
                 mService.onMessageChannelReady(mSessionBinder, extras);
@@ -91,6 +132,11 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
             }
         }
         return true;
+    }
+
+    @Override
+    public final boolean onPostMessage(String message, Bundle extras) {
+        return postMessage(message, extras);
     }
 
     /**
@@ -103,7 +149,7 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
      * @return Whether the postMessage was sent to the remote successfully.
      */
     public final boolean postMessage(String message, Bundle extras) {
-        if (mService == null) return false;
+        if (!isBoundToService()) return false;
         synchronized (mLock) {
             try {
                 mService.onPostMessage(mSessionBinder, message, extras);
@@ -114,13 +160,28 @@ public abstract class PostMessageServiceConnection implements ServiceConnection 
         return true;
     }
 
+    @Override
+    public void onDisconnectChannel(Context appContext) {
+        unbindFromContext(appContext);
+    }
+
     /**
      * Called when the {@link PostMessageService} connection is established.
      */
-    public void onPostMessageServiceConnected() {}
+    public void onPostMessageServiceConnected() {
+        if (mMessageChannelCreated) notifyMessageChannelReadyInternal(null);
+    }
 
     /**
      * Called when the connection is lost with the {@link PostMessageService}.
      */
     public void onPostMessageServiceDisconnected() {}
+
+    /**
+     * Cleans up any dependencies that this handler might have.
+     * @param context Context to use for unbinding if necessary.
+     */
+    public void cleanup(Context context) {
+        if (isBoundToService()) unbindFromContext(context);
+    }
 }
