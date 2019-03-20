7880674f2d4678bb66749321979796f3ce291f4a
Peter E Conn <peconn@chromium.org>
2019-03-20 10:47:22 +0000

    🛃 Clear service after unbinding.
    Ran into this crash a few times while developing. We try to unbind the
    service twice in a row.
    
    Change-Id: I9ef0d71da38584c9e81d938940719ff0901950c8

--- customtabs/tests/src/android/support/customtabs/PostMessageServiceConnectionTest.java
+++ customtabs/tests/src/android/support/customtabs/PostMessageServiceConnectionTest.java
@@ -18,21 +18,15 @@ package android.support.customtabs;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import android.content.Context;
-import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.rule.ServiceTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.TimeoutException;
 
 /**
  * Tests for {@link PostMessageServiceConnection} with no {@link CustomTabsService} component.
@@ -40,27 +34,19 @@ import java.util.concurrent.TimeoutException;
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class PostMessageServiceConnectionTest {
-    @Rule
-    public final ServiceTestRule mServiceRule;
-    @Rule
-    public final ActivityTestRule<TestActivity> mActivityTestRule;
     private TestCustomTabsCallback mCallback;
     private Context mContext;
     private PostMessageServiceConnection mConnection;
     private boolean mServiceConnected;
 
-
-    public PostMessageServiceConnectionTest() {
-        mActivityTestRule = new ActivityTestRule<TestActivity>(TestActivity.class);
-        mServiceRule = new ServiceTestRule();
-    }
-
     @Before
     public void setup() {
         mCallback = new TestCustomTabsCallback();
-        mContext = mActivityTestRule.getActivity();
-        mConnection = new PostMessageServiceConnection(
-                new CustomTabsSessionToken(mCallback.getStub(), null)) {
+        mContext = InstrumentationRegistry.getContext();
+
+        CustomTabsSessionToken token = new CustomTabsSessionToken(mCallback.getStub(), null);
+        mConnection = new PostMessageServiceConnection(token) {
+            @Override
             public void onPostMessageServiceConnected() {
                 mServiceConnected = true;
             }
@@ -70,29 +56,26 @@ public class PostMessageServiceConnectionTest {
                 mServiceConnected = false;
             }
         };
-        Intent intent = new Intent();
-        intent.setClassName(mContext.getPackageName(), PostMessageService.class.getName());
-        try {
-            mServiceRule.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-        } catch (TimeoutException e) {
-            fail();
-        }
+
+        mConnection.bindSessionToPostMessageService(mContext, mContext.getPackageName());
+        PollingCheck.waitFor(() -> mServiceConnected);
     }
 
     @Test
     public void testNotifyChannelCreationAndSendMessages() {
-        PollingCheck.waitFor(500, new PollingCheck.PollingCheckCondition() {
-            @Override
-            public boolean canProceed() {
-                return mServiceConnected;
-            }
-        });
-        assertTrue(mServiceConnected);
         mConnection.notifyMessageChannelReady(null);
         assertTrue(mCallback.isMessageChannelReady());
+
         mConnection.postMessage("message1", null);
         assertEquals(mCallback.getMessages().size(), 1);
+
         mConnection.postMessage("message2", null);
         assertEquals(mCallback.getMessages().size(), 2);
+    }
+
+    @Test
+    public void dontUnbindTwice() throws Throwable {
+        mConnection.cleanup(mContext);
+        mConnection.cleanup(mContext);
     }
 }
