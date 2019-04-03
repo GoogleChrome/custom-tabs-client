90ff6ef2fabb9765cb611ed2ced6be2da8a4fbc3
Pavel Shmakov <pshmakov@google.com>
2019-04-03 17:54:53 +0100

    Use a more flexible method of retrieving splash image from resources
    BitmapFactory#decodeBitmap doesn't work, for example, with layer
    drawables or vector drawables. The method drawableToBitmap added here
    should cover wider variety of drawable formats and sdk versions.
    
    Bug: 856544
    Change-Id: I3467fee1d7cb5e02730dd5bae9ad79913fc61e09
    Reviewed-on: https://chromium-review.googlesource.com/c/custom-tabs-client/+/1550500
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>

--- customtabs/src/android/support/customtabs/trusted/SplashImageTransferTask.java
+++ customtabs/src/android/support/customtabs/trusted/SplashImageTransferTask.java
@@ -6,12 +6,18 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsSession;
 import android.support.customtabs.TrustedWebUtils;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,6 +27,8 @@ import java.io.OutputStream;
  * Saves the splash image to a file and transfers it to Custom Tabs provider.
  */
 public class SplashImageTransferTask {
+
+    private static final String TAG = "SplashImageTransferTask";
 
     private static final String FOLDER_NAME = "twa_splash";
     private static final String FILE_NAME = "splash_image.png";
@@ -81,7 +89,8 @@ public class SplashImageTransferTask {
             if (!dir.exists()) {
                 boolean mkDirSuccessful = dir.mkdir();
                 if (!mkDirSuccessful) {
-                    throw new RuntimeException("Failed to make a directory for splash screens");
+                    Log.w(TAG, "Failed to create a directory for storing a splash image");
+                    return false;
                 }
             }
             File file = new File(dir, FILE_NAME);
@@ -94,8 +103,11 @@ public class SplashImageTransferTask {
                 return transferToCustomTabsProvider(file);
             }
             try(OutputStream os = new FileOutputStream(file)) {
-                Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), mResourceId);
-
+                Bitmap bitmap = drawableToBitmap(mContext, mResourceId);
+                if (bitmap == null) {
+                    Log.w(TAG, "Failed to retrieve a bitmap from the given resource id");
+                    return false;
+                }
                 if (isCancelled()) return false;
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
                 os.flush();
@@ -130,6 +142,23 @@ public class SplashImageTransferTask {
             }
         }
     };
+
+    @Nullable
+    private static Bitmap drawableToBitmap(Context context, int drawableId) {
+        Drawable drawable = ContextCompat.getDrawable(context, drawableId);
+        if (drawable == null) {
+            return null;
+        }
+        drawable = DrawableCompat.wrap(drawable);
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
 
     /** Callback to be called when the file is saved and transferred to Custom Tabs provider. */
     public interface Callback {
