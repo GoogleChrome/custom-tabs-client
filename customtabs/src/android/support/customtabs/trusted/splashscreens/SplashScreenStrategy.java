0f7c5699d2518b019df66905e0f8d9c3be09a83f
Pavel Shmakov <pshmakov@google.com>
2019-04-30 13:58:47 +0100

    Move general-purpose code out of LauncherActivity
    Two classes are factored out:
    - TwaLauncher encapsulates launching TWA, including connecting to
    service, etc.
    - PwaWrapperSplashScreenStrategy, which contains all the code managing
    splash screens (except for the decision of whether to show them).
    
    What remains in LauncherActivity:
    - Usage of TwaLauncher and SplashScreenStrategy, including destroying them.
    - Showing toast to update Chrome. I don't think this should be part of TwaLauncher
    or any other general-purpose class.
    - Code ensuring we finish LauncherActivity when TWA finishes. This is specific to
    TWA as PWA wrapper use case.
    - A few protected methods, such as getLaunchingUrl().
    
    Change-Id: Ia87b24ac82710e163edb263fa94067dfc2ecb771

---
+++ customtabs/src/android/support/customtabs/trusted/splashscreens/SplashScreenStrategy.java
@@ -0,0 +1,35 @@
+package android.support.customtabs.trusted.splashscreens;
+
+import android.support.annotation.Nullable;
+import android.support.customtabs.CustomTabsSession;
+import android.support.customtabs.trusted.TrustedWebActivityBuilder;
+
+/**
+ * Defines behavior of the splash screen shown when launching a TWA.
+ */
+public interface SplashScreenStrategy {
+
+    /**
+     * Called immediately in the beginning of TWA launching process (before establishing
+     * connection with CustomTabsService). Can be used to display splash screen on the client app's
+     * side before the browser is launched.
+     * @param providerPackage Package name of the browser being launched. Implementations should
+     * check whether this browser supports splash screens.
+     * @param statusBarColor Status bar color of TWA. Implementations that show splash screen in
+     * client app should set this status bar color. Null if client did not specify a status bar
+     * color.
+     */
+    void onTwaLaunchInitiated(String providerPackage, @Nullable Integer statusBarColor);
+
+    /**
+     * Called when TWA is ready to be launched.
+     * @param builder {@link TrustedWebActivityBuilder} to be supplied with splash screen related
+     * parameters.
+     * @param session {@link CustomTabsSession} with which the TWA will launch.
+     * @param onReadyCallback Callback to be triggered when splash screen preparation is finished.
+     * TWA is launched immediately upon triggering this callback.
+     */
+    void configureTwaBuilder(TrustedWebActivityBuilder builder, CustomTabsSession session,
+            Runnable onReadyCallback);
+
+}
