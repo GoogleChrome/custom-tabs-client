2ce08c688daeb8d12743e7810bb08d0a59561c76
Pavel Shmakov <pshmakov@google.com>
2019-08-07 15:57:15 +0100

    Web Share Target for TWA, client side
    Browser side: https://crrev.com/c/1724686
    
    Bug: 985331
    Change-Id: Ide1d260763af3d0416ce17e1782952f9540b9a2e

---
+++ customtabs/src/android/support/customtabs/trusted/TrustedWebActivityIntent.java
@@ -0,0 +1,56 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package android.support.customtabs.trusted;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+
+import java.util.List;
+
+/**
+ * Holds an {@link Intent} and other data necessary to start a Trusted Web Activity.
+ */
+public class TrustedWebActivityIntent {
+    @NonNull
+    private final Intent mIntent;
+    @Nullable
+    private final List<Uri> mSharedFileUris;
+
+    TrustedWebActivityIntent(@NonNull Intent intent,
+            @Nullable List<Uri> sharedFileUris) {
+        mIntent = intent;
+        mSharedFileUris = sharedFileUris;
+    }
+
+    /**
+     * Launches a Trusted Web Activity.
+     */
+    public void launchTrustedWebActivity(Context context) {
+        grantUriPermissionToProvider(context);
+        ContextCompat.startActivity(context, mIntent, null);
+    }
+
+    private void grantUriPermissionToProvider(Context context) {
+        if (mSharedFileUris == null) {
+            return;
+        }
+        for (Uri uri : mSharedFileUris) {
+            context.grantUriPermission(mIntent.getPackage(), uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+    }
+
+    /**
+     * Returns the held {@link Intent}. For launching a Trusted Web Activity prefer using
+     * {@link #launchTrustedWebActivity}.
+     */
+    public Intent getIntent() {
+        return mIntent;
+    }
+}
